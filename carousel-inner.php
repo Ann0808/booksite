a26402bcbd3d9d5f399890cea99401cc7e585209
Ann0808 <an.piskunova2013@yandex.ru>
2017-11-25 10:10:19 +0300

    background for books

--- carousel-inner.php
+++ carousel-inner.php
@@ -1,5 +1,5 @@
 <div class="carousel-inner row w-100 mx-auto" role="listbox">
-    <div class="carousel-item col-md-4 col-lg-3 active">
+    <div class="carousel-item  col-md-4 col-lg-3 active">
         <img class="img-fluid mx-auto d-block" src="/img/book.jpg" alt="slide 1">
     </div>
     <div class="carousel-item col-md-4 col-lg-3">
