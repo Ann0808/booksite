2412a2266f87e40424d17341405729da4404f685
Ann0808 <an.piskunova2013@yandex.ru>
2017-11-25 10:12:23 +0300

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,76 @@
+module.exports = function(grunt) {
+
+grunt.initConfig({
+
+less: {
+development: {
+options: {
+compress: false,
+yuicompress: false,
+optimization: 2
+},
+
+files: {
+"css/style.css": ['less/*.less', 'less/blocks/*.less'],
+}
+}
+},
+watch: {
+scripts: {
+files: ['script/*.js'],
+tasks: ['concat:main', 'jshint', 'uglify'],
+options: {
+spawn: false,
+reload: true
+}
+},
+css: {
+files: ['less/*.less', 'less/blocks/*.less'],
+tasks: ['less'],
+options: {
+spawn: false,
+reload: true
+}
+
+}
+},
+
+imagemin: {
+dynamic: {
+files: [{
+expand: true,
+cwd: '',
+src: ['img/*.{png,jpg,gif}'],
+dest: ''
+}]
+}
+},
+
+postcss: {
+options: {
+processors: [
+require('autoprefixer')({browsers: ['last 2 version']})
+]
+},
+dist: {
+src: 'css/style.css',
+dest: 'css/style.css'
+}
+}
+
+
+
+
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-postcss');
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+
+grunt.registerTask('default', ['imagemin', 'postcss']);
+};
