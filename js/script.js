433be2e48457399000b62e3479343b82f6015677
Ann0808 <an.piskunova2013@yandex.ru>
2017-11-28 20:13:16 +0300

    scroll top

--- js/script.js
+++ js/script.js
@@ -35,18 +35,19 @@ $(window).scroll(function() {
   }
   $('.button-to-top').click(function() {
 
-    const scrollHeight = window.scrollY,
-      scrollStep = Math.PI / (1000 / 15),
-      cosParameter = scrollHeight / 2;
-    var scrollCount = 0,
-      scrollMargin,
-      scrollInterval = setInterval(function() {
-        if (window.scrollY != 0) {
-          scrollCount = scrollCount + 1;
-          scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
-          window.scrollTo(0, (scrollHeight - scrollMargin));
-        } else clearInterval(scrollInterval);
-      }, 15);
+    // const scrollHeight = window.scrollY,
+    //   scrollStep = Math.PI / (1000 / 15),
+    //   cosParameter = scrollHeight / 2;
+    // var scrollCount = 0,
+    //   scrollMargin,
+    //   scrollInterval = setInterval(function() {
+    //     if (window.scrollY != 0) {
+    //       scrollCount = scrollCount + 1;
+    //       scrollMargin = cosParameter - cosParameter * Math.cos(scrollCount * scrollStep);
+    //       window.scrollTo(0, (scrollHeight - scrollMargin));
+    //     } else clearInterval(scrollInterval);
+    //   }, 15);
+     $(window).scrollTop(0);
   });
 });
 
