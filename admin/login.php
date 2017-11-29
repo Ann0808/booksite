74a96015ed974882c7469e3cc840c55f6bcb73c4
Mikhail <vms2108@mail.ru>
2017-11-29 22:45:33 +0300

    new style for admin-page and admin-login

--- admin/login.php
+++ admin/login.php
@@ -15,39 +15,35 @@ header( "Location: admin.php" );
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script> -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <!--<link rel="stylesheet" href="/css/bootstrap.min.css">-->
     <script src="/js/jquery-3.2.1.min.js"></script>
     <script src="/js/popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="/css/style.css">
     <script type="text/javascript" src="/js/script.js"></script>
 </head>
-<body>
-  <form action="admin.php?action=login" method="post" style="width: 50%;">
-        <input type="hidden" name="login" value="true" />
+<body class="admin__body">
+	<div class="admin-container">
+		<div class="admin-container__items">
+				<form action="admin.php?action=login" method="post">
+					<input type="hidden" name="login" value="true" />
+									<div class="admin-container__row">
+											<p><label for="username">Username</label></p>
+											<input class="admin-container__input-text" type="text" name="username" id="username" placeholder="Your admin username" required autofocus maxlength="20" />
+									</div>
+									<div class="admin-container__row">
+												<p><label for="password">Password</label></p>
+												<input class="admin-container__input-text"  type="password" name="password" id="password" placeholder="Your admin password" required maxlength="20" />
+									</div>
+									<div class="admin-container__row">
+												<input class="btn" type="submit" name="login" value="Login" />
+												<?php if ( isset( $results['errorMessage'] ) ) { ?>
+																<div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
+												<?php } ?>
+									</div>
+				</form>
+		</div>
 
-<?php if ( isset( $results['errorMessage'] ) ) { ?>
-        <div class="errorMessage"><?php echo $results['errorMessage'] ?></div>
-<?php } ?>
-
-        <ul>
-
-          <li>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username" placeholder="Your admin username" required autofocus maxlength="20" />
-          </li>
-
-          <li>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Your admin password" required maxlength="20" />
-          </li>
-
-        </ul>
-
-        <div class="buttons">
-          <input type="submit" name="login" value="Login" />
-        </div>
-
-      </form>
+	</div>
 </body>
 </html>
