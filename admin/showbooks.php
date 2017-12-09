d1745a3fa5caaea5dd558fc2380d97c0bacf40ed
Mikhail <vms2108@mail.ru>
2017-12-09 21:28:12 +0300

    img adapted

--- admin/showbooks.php
+++ admin/showbooks.php
@@ -46,4 +46,35 @@ $everything = Book::queryMysql("SELECT * FROM books"); ?>
    	</div>
    </div>
 
+   <script>
+		 jQuery(function ($) {
+				function fix_size() {
+						var images = $('.col img');
+						images.each(setsize);
+
+						function setsize() {
+								var img = $(this),
+										img_dom = img.get(0),
+										container = img.parents('.col');
+								if (img_dom.complete) {
+										resize();
+								} else img.one('load', resize);
+
+								function resize() {
+										if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+												img.width('100%');
+												img.height('auto');
+												return;
+										}
+										img.height('100%');
+										img.width('auto');
+								}
+						}
+				}
+				$(window).on('resize', fix_size);
+				fix_size();
+		});
+
+	</script>
+
 <?php include("footer_admin.php"); ?>
--- index.php
+++ index.php
@@ -142,5 +142,35 @@ if ($mysqli->connect_error) die($mysqli->connect_error);
   </main>
   <script type="text/javascript" src="/js/script.js"></script>
   <script type="text/javascript" src="/js/carousel.js"></script>
+  <script>
+		 jQuery(function ($) {
+				function fix_size() {
+						var images = $('.carousel__item img');
+						images.each(setsize);
+
+						function setsize() {
+								var img = $(this),
+										img_dom = img.get(0),
+										container = img.parents('.carousel__item');
+								if (img_dom.complete) {
+										resize();
+								} else img.one('load', resize);
+
+								function resize() {
+										if ((container.width() / container.height()) < (img_dom.width / img_dom.height)) {
+												img.width('100%');
+												img.height('auto');
+												return;
+										}
+										img.height('100%');
+										img.width('auto');
+								}
+						}
+				}
+				$(window).on('resize', fix_size);
+				fix_size();
+		});
+
+	</script>
 </body>
 </html>
