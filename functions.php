472b66f9d2ca89c81991058a7428b30de3092f03
Mikhail <vms2108@mail.ru>
2017-12-24 11:29:37 +0300

    new data in functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
-  $dbhost  = 'localhost';
-  $dbname  = 'cms';
-  $dbuser  = 'root';
-  $dbpass  = '';
+ require_once( "config.php" );
+ $dbhost  = 'localhost';
+ $dbname  = 'cms';
+ $dbuser  = DB_USERNAME;
+ $dbpass  = DB_PASSWORD;
 
   $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
   if ($connection->connect_error) die($connection->connect_error);
