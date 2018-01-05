70ac60f43ecf28fc8ab8f26eff8b16cefc523fbc
Ann0808 <an.piskunova2013@yandex.ru>
2018-01-05 14:09:25 +0300

    отображается название книги

--- admin/showpurch.php
+++ admin/showpurch.php
@@ -3,34 +3,43 @@ session_start();
 ?>
 <?php if ( $_SESSION['adminname'] == $adminLogin) { ?>
 <?php include("header_admin.php");
-$everything = Book::queryMysql("SELECT members.name, members.user, books.name, books.price
+$everything = Book::queryMysql("SELECT  members.name, members.user , books.name AS n, books.price 
 FROM purchases
 INNER JOIN members ON purchases.id_member =members.id
-INNER JOIN books ON purchases.id_book =books.id;");
+INNER JOIN books ON purchases.id_book =books.id
+;");
 
 ?>
+
+<?  foreach ($everything as $value) { ?>
+
+        <?php foreach ($value as $v) { ?>
+
+           <?php echo $v ?>
+
+        <? }  ?>
+
+  <? } ?>
 
 <div class="admin-container">
  <div class="admin-container__items">
    <div class="admin-container__item">
    <h2>Basic Table</h2>
      <div class="admin-container__row start">
-       <div class="purchases-col border-top">UserNAme</div>
-       <div class="purchases-col border-top">UserLogin</div>
-       <div class="purchases-col border-top">BookName</div>
-       <div class="purchases-col border-top">BookCost</div>
+       <div class="purchases-col border-top">Имя пользователя</div>
+       <div class="purchases-col border-top">Логин пользователя</div>
+       <div class="purchases-col border-top">Название книги</div>
+          <div class="purchases-col border-top">Цена книги</div>
      </div>
-     <?
-         foreach ($everything as $value) { ?>
+     <?  foreach ($everything as $value) { ?>
            <div class="admin-container__row admin-container__row--big start">
              <?php foreach ($value as $v) { ?>
                <div class="purchases-col">
                 <?php echo $v ?>
                </div>
              <? }  ?>
-
          </div>
-       <?  } ?>
+       <? } ?>
    </div>
  </div>
 </div>
