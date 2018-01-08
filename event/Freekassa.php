396ac428aec3589dfbde7b9967dbd0f85b2c4842
Ann0808 <an.piskunova2013@yandex.ru>
2018-01-08 13:40:52 +0300

    при покупке отправляется письмо

--- event/Freekassa.php
+++ event/Freekassa.php
@@ -3,6 +3,9 @@
 require( "settings.php" );
 $id_user = $_POST['us_login'];
 $id_book = $_POST['us_book'];
+$searchMail = Book::queryMysql("SELECT email from members WHERE id='$id_user'");
+$searchMail = $searchMail->fetch_array(MYSQLI_ASSOC);
+$usermail = $searchMail['email'];
 
 function getIP() {
     if(isset($_SERVER['HTTP_X_REAL_IP'])) return $_SERVER['HTTP_X_REAL_IP'];
@@ -14,6 +17,13 @@ function getIP() {
 
 Book::queryMysql("INSERT INTO `purchases`(`id_book`, `id_member`) VALUES ('$id_book','$id_user')");
 
+$query = Book::queryMysql("SELECT name, link from books WHERE id='$id_book'");
+$query=$query->fetch_array(MYSQLI_ASSOC);
+$bookname = $query['name'];
+$booklink = $query['link'];
+$link =  $_SERVER['HTTP_HOST'] . "/page.php?chapter=0&book=" . $booklink;
+$textMail = "Здравствуйте!\nБлагодарим вас за покупку книги " . $bookname . "\nСсылка на Вашу книгу: ". $link;
+mail($usermail, "Спасибо за покупку", $textMail);
 
 exit();
 
