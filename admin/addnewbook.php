c0e88417be456408225dcfc06aded82f1b249893
Mikhail <vms2108@mail.ru>
2018-01-10 22:37:12 +0700

    новая рекомендация

--- admin/addnewbook.php
+++ admin/addnewbook.php
@@ -8,7 +8,7 @@ session_start(); ?>
 
                 <form enctype="multipart/form-data" method="post" action="addnewbook.php">
                     <div class="admin-container__block">
-                        <span class="admin-container__addition">Все изображения должны быть в формате jpeg</span>
+                        <span class="admin-container__addition">Все изображения должны быть в формате jpg и предварительно сжаты (для сжатия воспользуйтесь ссылкой http://optimizilla.com/ru/)</span>
                         <div class="admin-container__row">
                             <p>Логотип для мобильной версии
                             <br><span class="admin-container__addition">Изображение должно быть в горизонтальной ориентации. </span></p>
