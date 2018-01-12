5d834dfef3732a626f48b636939698c1aa2728cb
Ann0808 <an.piskunova2013@yandex.ru>
2018-01-12 17:30:35 +0300

    style.min.css

--- admin/header_admin.php
+++ admin/header_admin.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin panel</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/style.min.css">
 		<script src="../js/jquery-3.2.1.min.js"></script>
 
   </head>
